974fa9448ed8baff21263ca287ce3eeaa9cbf42d
EFI <erfanseif.work@gmail.com>
2023-01-25 17:19:20 +0330

    take the inputs & print it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome To The Guessing Game!");
+    println!("What's Your Guess?  ");
+    
+    let mut guess = String::new();
+    std::io::stdin()
+        .read_line( &mut guess)
+        .expect("Failed To Read Line...");
+
+    println!("You Guessed: {}" , guess);
+
+
 }
