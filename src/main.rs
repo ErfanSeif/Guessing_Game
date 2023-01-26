3af9898953464672d96d2e6bf94f2e17eb0b37d0
EFI <erfanseif.work@gmail.com>
2023-01-26 15:00:30 +0330

    use rand Crate to make random number

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
     println!("Welcome To The Guessing Game!");
-    println!("What's Your Guess?  ");
-    
+    //---    
+
+    let randNumber = rand::thread_rng().gen_range(1..=100);
+    println!("Random Number Is: {}",randNumber);
+    //---    
+
     let mut guess = String::new();
+
+    println!("What's Your Guess?  ");
+
     std::io::stdin()
         .read_line( &mut guess)
         .expect("Failed To Read Line...");
 
     println!("You Guessed: {}" , guess);
-    
+    //---    
 
 }
